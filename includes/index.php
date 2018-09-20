984814658c731d279ec6e75d39a2274e15e06853
Antonio de Leon <antoniodeleon@R2D2-3.local>
2018-09-19 23:38:16 -0400

    added name, photo, random quote (hard code), and footer

---
+++ includes/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Project 1: About Me</title>
+    <link rel="stylesheet" href="css/main.css" type="text/css" />
+  </head>
+  <body>
+    <section>
+      <h1>Venicio Antonio Vicente de Leon</h1>
+        <img src="css/images/venicioantoniovicentedeleon.jpg" alt="Photo of Antonio" id="selfphoto">
+        <h2>About Me</h2>  
+        <p>My name is Antonio de Leon. It's definitely a pleasure to meet you all from around the world. I'm currently an ALB degree candidate studying computer science. Currently, I'm taking three other courses (Data Structures and two other non-CS courses). I plan on applying to tech companies as a product manager and hope this course will strengthen my back-end skills and make me a more competitive candidate. I'm definitely looking forward to learn with you all!<br />
+        Antonio de Leon</p>
+        <?php include("includes/navigation.php"); ?>
+        <!-- delete this -->
+        <h2>Random Quote</h2>
+        <p id="quote">"Test Quote"<p>
+        <!-- delete this -->
+        
+        <img src="css/images/circle.png" alt="circle" id="circlebtn">
+
+    </section>
+    <footer>
+      <p>Venicio Antonio de Leon | CSCI E-15 Dynamic Web Applications | Project 1</p>
+    </footer>
+  </body>
+</html>
